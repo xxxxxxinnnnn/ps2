f6e13c8a74742c431714ed19cac3c5c2bce0d655
echo <echo@echodeair.fios-router.home>
2017-09-20 23:10:02 -0400

    code

---
+++ ps2/src/test/java/base/MyInteger_Test.java
@@ -0,0 +1,78 @@
+package base;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyInteger_Test {
+	
+	MyInteger odd_num = new MyInteger(5);
+	MyInteger even_num = new MyInteger(4);
+	MyInteger prime_num = new MyInteger(2);
+
+	@Test
+	public void testEven() {
+		assertTrue(even_num.isEven());
+	}
+
+	@Test
+	public void testOdd() {
+		assertTrue(odd_num.isOdd());
+	}
+
+	@Test
+	public void testPrime() {
+		assertTrue(prime_num.isPrime());
+	}
+
+	@Test
+	public void testEvenInt() {
+		MyInteger myInt = new MyInteger(2);
+		assertTrue(myInt.isEven());
+	}
+
+	@Test
+	public void testOddInt() {
+		MyInteger myInt = new MyInteger(5);
+		assertTrue(myInt.isOdd());
+	}
+
+	@Test
+	public void testPrimeInt() {
+		MyInteger myInt = new MyInteger(3);
+		assertTrue(myInt.isPrime());
+	}
+
+	@Test
+	public void testEqualInt() {
+		MyInteger myInt = new MyInteger(10);
+		assertTrue(myInt.isEquals(10));
+	}
+
+	@Test
+	public void testEvenMyInteger() {
+		MyInteger myInt = new MyInteger(4);
+		assertTrue(myInt.isEven(even_num));
+	}
+
+	@Test
+	public void testOddMyInteger() {
+		MyInteger myInt = new MyInteger(5);
+		assertTrue(myInt.isOdd(odd_num));
+	}
+
+	@Test
+	public void testPrimeMyInteger() {
+		MyInteger myInt = new MyInteger(11);
+		assertTrue(myInt.isPrime(prime_num));
+	}
+
+	@Test
+	public void testEqualMyInteger() {
+		MyInteger myInt = new MyInteger(4);
+		MyInteger myInt2 = new MyInteger(4);
+
+		assertTrue(myInt.isEquals(myInt2));
+	}
+
+}
---
+++ ps2/src/test/resources/packageEmpty/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author echo
+ *
+ */
+package packageEmpty;
